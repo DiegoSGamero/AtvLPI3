13eed424ae9d3db00c52b2c3bdec3dde934bd88e
Diego Sales <diegosgjoao@gmail.com>
2024-11-06 21:16:47 -0300

    class feline implemented

--- src/main/java/org/example/atvlpi3/models/Feline.java
+++ src/main/java/org/example/atvlpi3/models/Feline.java
@@ -1,4 +1,55 @@
 package org.example.atvlpi3.models;
 
-public class Feline {
+public class Feline implements Animal {
+    private String species;
+    private String habitat;
+    private double weight;
+    private double maxSpeed;
+
+    // Construtor
+    public Feline(String species, String habitat, double weight, String furColor, double maxSpeed) {
+        this.species = species;
+        this.habitat = habitat;
+        this.weight = weight;
+        this.maxSpeed = maxSpeed;
+    }
+
+    // Métodos da interface
+    @Override
+    public String getSpecies() {
+        return species;
+    }
+
+    @Override
+    public String getHabitat() {
+        return habitat;
+    }
+
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+
+    // Métodos adicionais da classe Feline
+    public void makeSound() {
+        System.out.println("O felino está rugindo ou miando!");
+    }
+
+    public void move() {
+        System.out.println("O felino está se movendo rapidamente com uma velocidade de " + maxSpeed + " km/h.");
+    }
+
+    public void feed() {
+        System.out.println("O felino está se alimentando de carne.");
+    }
+
+    // Getters e setters
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(double maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
 }
