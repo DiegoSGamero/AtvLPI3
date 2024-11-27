55fff11d0d9a1b547479607d2421b99c9ab7ac9d
Diego Sales <diegosgjoao@gmail.com>
2024-11-27 20:15:02 -0300

    starting automovel interface task and images added

---
+++ src/main/java/org/example/atvlpi3/models/Automovel.java
@@ -0,0 +1,7 @@
+package org.example.atvlpi3.models;
+
+public interface Automovel {
+    void ligar();
+    void acelerar();
+    void freiar();
+}
---
+++ src/main/java/org/example/atvlpi3/models/Carro.java
@@ -0,0 +1,4 @@
+package org.example.atvlpi3.models;
+
+public class Carro {
+}
